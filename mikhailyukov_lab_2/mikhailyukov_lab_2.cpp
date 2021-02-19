36a79346aa5033bf88d186d16e201cfb2a98ad83
Олег Михайлюков <mihaylyukov22@mail.ru>
2021-02-19 14:49:36 +0600

    переделал программу для подсчёта суммы конкретного числа элементов ряда

--- mikhailyukov_lab_2/mikhailyukov_lab_2.cpp
+++ mikhailyukov_lab_2/mikhailyukov_lab_2.cpp
@@ -1,12 +1,12 @@
 ﻿#include <iostream>
 #include <cmath>
-#define epsilon 0.0000001
+#define elements_count 200
 using namespace std;
 
-//данная программа считает сумму ряда вида: 1 - x^2 / 3! + x^4 / 5! - ... для введённого x и указанной границы epsilon
+//данная программа считает сумму ряда вида: 1 - x^2 / 3! + x^4 / 5! - ... для введённого x для elements_count элементов последовательности
 
 double get_number();
-double calculate_series_sum(const double x, int* summands_count);
+double calculate_series_sum(const double x);
 
 int main()
 {
@@ -15,23 +15,21 @@ int main()
     cout << "укажите число x: ";
     const double x = get_number();
     
-    int summands_count = 0;
-    const double series_sum = calculate_series_sum(x, &summands_count);
+    const double series_sum = calculate_series_sum(x);
 
-    cout << "сумма ряда для указанного числа x: " << series_sum << ", элементов в посчитанной сумме: " << summands_count;
+    cout << "сумма " << elements_count << " элементов ряда для указанного числа x: " << series_sum;
 
     return 0;
 }
 
 
-double calculate_series_sum(const double x, int* summands_count)
+double calculate_series_sum(const double x)
 {
     double summand = 1;
     double series_sum = 0;
     int d = 1;
-    while (fabs(summand) > epsilon) {
+    for (int i = 0; i < elements_count; i++) {
         series_sum += summand;
-        *summands_count += 1;
         d += 2;
         summand *= -1 * x * x / d / (d - 1);
     }
